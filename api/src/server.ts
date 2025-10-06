14fec5058d2a0850869f13f852913ac2784caddf
karan <xkaranx5@gmail.com>
2025-10-06 13:58:51 +0530

    Refactor server setup to use TypeScript types, update port configuration, and add health check endpoint; create package.json for project dependencies and scripts

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,11 +1,26 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
 
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
+
+app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!')
+})
+
+
+
+app.get('/health', (req: Request, res: Response) => {
+  res.send('OK')
+})
+
+app.use((req: Request, res: Response) => {
+  const pathName = req.path
+  res.send(pathName)
+  // res.redirect('/')
 })
 
 app.listen(port, () => {
