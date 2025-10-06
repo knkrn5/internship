afa2ce4baa8389734d1986f5510fa0375fbec1cc
karan <xkaranx5@gmail.com>
2025-10-06 14:39:51 +0530

    Add CORS support, update TypeScript configuration, and introduce Prettier configuration

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,10 +1,28 @@
 import express, { Request, Response } from 'express'
+import cors from 'cors'
 import dotenv from 'dotenv'
-dotenv.config()
+dotenv.config({ quiet: true })
 
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8000
+
+
+const dynamicCorsOptions = function (req: Request, callback: (err: any, options?: cors.CorsOptions) => void) {
+  let corsOptions;
+  if (req.path.startsWith('/auth/')) {
+    corsOptions = {
+      origin: 'http://mydomain.com',
+      credentials: true,
+    };
+  } else {
+    corsOptions = { origin: '*' };
+  }
+  callback(null, corsOptions);
+};
+
+
+app.use(cors(dynamicCorsOptions))
 
 
 app.get('/', (req: Request, res: Response) => {
@@ -12,15 +30,13 @@ app.get('/', (req: Request, res: Response) => {
 })
 
 
-
 app.get('/health', (req: Request, res: Response) => {
   res.send('OK')
 })
 
 app.use((req: Request, res: Response) => {
-  const pathName = req.path
-  res.send(pathName)
-  // res.redirect('/')
+  const { method, path, originalUrl, query } = req
+  res.json({ method, path, originalUrl, query })
 })
 
 app.listen(port, () => {
